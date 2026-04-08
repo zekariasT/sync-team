af6aa2320024f0bc5994978a380ba314d34677a1
Zekarias Tesfaye <zakmc117@gmail.com>
2026-04-08 12:28:03 +0300

    chore: add comment regarding environment configuration in middleware matcher

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -8,6 +8,7 @@ export default clerkMiddleware(async (auth, request) => {
   }
 });
 
+//changed env
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
